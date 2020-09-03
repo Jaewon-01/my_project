44930b4ca3a9eb466f34c0788908043a8ee8b26e
Jaewon-01 <jae4690@naver.com>
2020-09-03 21:36:30 +0900

    direction_rollback1.0
    direction 경로 찾는건 아무래도 다른 방법을 찾아봐야 할것같아 롤백중입니다

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스�
 
 app = Flask(__name__)
 
+
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만듭니다.
 
 
 
-## HTML을 주는 부분
+
+
+
+#HTML을 주는 부분
 @app.route('/')
 def home():
     return render_template('index.html')
